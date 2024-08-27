7ca6f95cc2cdf843867ce46bbf24b30f13cb7851
Safronov Igor <safronov@bestly.ru>
2024-08-27 16:19:57 +0300

    rest

--- gateway/src/main/java/ru/practicum/shareit/items/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/items/dto/ItemRequestDto.java
@@ -11,7 +11,6 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemRequestDto {
-    @NotBlank
     String name;
     @NotBlank
     String description;
