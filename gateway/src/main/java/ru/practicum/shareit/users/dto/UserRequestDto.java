6912a23ea38861206e925a219a134f2d79b705c8
Safronov Igor <SafronovIgor1C@yandex.ru>
2024-08-30 21:14:40 +0300

    test PR

--- gateway/src/main/java/ru/practicum/shareit/users/dto/UserRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/users/dto/UserRequestDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.users.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -12,7 +13,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserRequestDto {
-    @NotNull
+    @NotBlank
     String name;
     @NotNull
     @Email
